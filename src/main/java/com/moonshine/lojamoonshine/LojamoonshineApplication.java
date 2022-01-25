fe0a9d2e02d5f6d2adfd5591ce8a0368ea671dd9
costarafa <rafaelacs.rc@gmail.com>
2022-01-25 00:18:09 -0300

    first commit

---
+++ src/main/java/com/moonshine/lojamoonshine/LojamoonshineApplication.java
@@ -0,0 +1,13 @@
+package com.moonshine.lojamoonshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojamoonshineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojamoonshineApplication.class, args);
+	}
+
+}
